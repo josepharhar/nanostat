6fc893f2e5c70c3412296d49b4c3c78f4202c567
Joseph Arhar <josepharhar@gmail.com>
2019-02-18 21:50:17 -0800

    types dont work yet

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,5 @@ declare function stat(filepath: string, callback: (stats: Object) => void): void
 declare function lstat(filepath: string, callback: (stats: Object) => void): void;
 declare function statSync(filepath: string): Object;
 declare function lstatSync(filepath: string): Object;
+
+export = stat;
